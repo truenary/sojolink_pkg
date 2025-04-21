a925409bb52947ca1700527f89d933663f298d78
bikal <bikal.rumba@truenary.com>
2025-04-21 08:50:57 +0545

    feat: setup example app for plugin testing and demo

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -10,16 +10,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:sojo_link/sojo_link.dart';
-
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    final SojoLink plugin = SojoLink();
-    final String? version = await plugin.getPlatformVersion();
-    // The version string depends on the host platform running the test, so
-    // just assert that some non-empty string is returned.
-    expect(version?.isNotEmpty, true);
+  
+ 
   });
 }
