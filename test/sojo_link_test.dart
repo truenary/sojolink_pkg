a87e663979ee72beca3f2f828ccdbe5b02c48a58
uden.shrestha <uden.shrestha@truenary.com>
2025-06-16 15:41:55 +0545

    chore(pub): achieve 160/160 pub points with formatting and analysis fixes

--- test/sojo_link_test.dart
+++ test/sojo_link_test.dart
@@ -1,9 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
-
-  test('getPlatformVersion', () async {
-//TODO test implementation
-  });
+  test('getPlatformVersion', () async {});
 }
